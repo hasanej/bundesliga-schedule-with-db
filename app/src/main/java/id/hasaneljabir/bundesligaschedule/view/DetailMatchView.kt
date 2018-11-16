4e2f199d708d44291a37b5b706cf6de8094a421b
Hasan El Jabir <eljabirhasan@gmail.com>
2018-11-17 00:12:59 +0700

    HS : Optimize code

--- app/src/main/java/id/hasaneljabir/bundesligaschedule/view/DetailMatchView.kt
+++ app/src/main/java/id/hasaneljabir/bundesligaschedule/view/DetailMatchView.kt
@@ -4,14 +4,14 @@ import id.hasaneljabir.bundesligaschedule.model.TeamData
 
 interface DetailMatchView {
 
-    interface View{
+    interface View {
         fun showLogoHome(team: TeamData)
         fun showLogoAway(team: TeamData)
     }
 
-    interface Presenter{
-        fun getLogoHome(id:String)
-        fun getLogoAway(id:String)
+    interface Presenter {
+        fun getLogoHome(id: String)
+        fun getLogoAway(id: String)
         fun onDestroyPresenter()
     }
 }
