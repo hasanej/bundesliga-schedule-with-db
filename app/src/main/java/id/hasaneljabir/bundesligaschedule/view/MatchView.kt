e305c949df8f15870b4c0dcd980d583cc5ef125d
Hasan El Jabir <eljabirhasan@gmail.com>
2018-11-17 08:07:54 +0700

    HS : Update code

--- app/src/main/java/id/hasaneljabir/bundesligaschedule/view/MatchView.kt
+++ app/src/main/java/id/hasaneljabir/bundesligaschedule/view/MatchView.kt
@@ -3,13 +3,13 @@ package id.hasaneljabir.bundesligaschedule.view
 import id.hasaneljabir.bundesligaschedule.model.EventData
 
 interface MatchView {
-    interface View{
+    interface View {
         fun hideLoading()
         fun showLoading()
-        fun showMatchList(matchList:List<EventData>)
+        fun showMatchList(matchList: List<EventData>)
     }
 
-    interface Presenter{
+    interface Presenter {
         fun getMatchList(leagueName: String = "4331")
         fun onDestroyPresenter()
     }
