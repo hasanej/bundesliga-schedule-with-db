34f0f7ee7e7156921a3456aab1005dc821e76a0e
Hasan El Jabir <eljabirhasan@gmail.com>
2018-11-17 08:05:07 +0700

    HS : Add database helper for favorite match

--- app/src/main/java/id/hasaneljabir/bundesligaschedule/databaseHelper/MyDatabaseOpenHelper.kt
+++ app/src/main/java/id/hasaneljabir/bundesligaschedule/databaseHelper/MyDatabaseOpenHelper.kt
@@ -1,4 +1,4 @@
-package com.rahmat.app.footballclub.entity.db
+package id.hasaneljabir.bundesligaschedule.databaseHelper
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -19,19 +19,20 @@ class MyDatabaseOpenHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "Favorit
     }
 
     override fun onCreate(p0: SQLiteDatabase) {
+        // Here you create tables
         p0.createTable(
-            FavoriteTeam.TEAM_TABLE, true,
-            FavoriteTeam.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
-            FavoriteTeam.TEAM_ID to TEXT,
-            FavoriteTeam.TEAM_IMAGE to TEXT
+            FavoriteMatch.TABLE_FAVORITE, true,
+            FavoriteMatch.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+            FavoriteMatch.EVENT_ID to TEXT + UNIQUE,
+            FavoriteMatch.HOME_TEAM_ID to TEXT,
+            FavoriteMatch.AWAY_TEAM_ID to TEXT
         )
     }
 
     override fun onUpgrade(p0: SQLiteDatabase, p1: Int, p2: Int) {
-        p0.dropTable(FavoriteTeam.TEAM_TABLE, true)
+        p0.dropTable(FavoriteMatch.TABLE_FAVORITE, true)
     }
 }
 
-// Access property for Context
 val Context.database: MyDatabaseOpenHelper
     get() = MyDatabaseOpenHelper.getInstance(applicationContext)
