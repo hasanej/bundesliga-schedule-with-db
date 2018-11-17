fe7d4e10460492d5474595ed3f577520142ee237
Hasan El Jabir <eljabirhasan@gmail.com>
2018-11-17 08:06:33 +0700

    HS : Update code

--- app/src/main/java/id/hasaneljabir/bundesligaschedule/presenter/LastMatchPresenter.kt
+++ app/src/main/java/id/hasaneljabir/bundesligaschedule/presenter/LastMatchPresenter.kt
@@ -1,9 +1,9 @@
 package id.hasaneljabir.bundesligaschedule.presenter
 
-import id.hasaneljabir.bundesligaschedule.utils.SchedulerProvider
-import id.hasaneljabir.bundesligaschedule.repository.MatchRepositoryImplementation
-import id.hasaneljabir.bundesligaschedule.view.MatchView
 import id.hasaneljabir.bundesligaschedule.model.EventResponse
+import id.hasaneljabir.bundesligaschedule.repository.MatchRepositoryImplementation
+import id.hasaneljabir.bundesligaschedule.utils.SchedulerProvider
+import id.hasaneljabir.bundesligaschedule.view.MatchView
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subscribers.ResourceSubscriber
 import java.util.*
@@ -11,26 +11,34 @@ import java.util.*
 class LastMatchPresenter(
     val view: MatchView.View,
     val matchRepositoryImplementation: MatchRepositoryImplementation,
-    val scheduler: SchedulerProvider) : MatchView.Presenter {
+    val scheduler: SchedulerProvider
+) : MatchView.Presenter {
     val compositeDisposable = CompositeDisposable()
 
-    override fun onDestroyPresenter() { compositeDisposable.dispose() }
+    override fun onDestroyPresenter() {
+        compositeDisposable.dispose()
+    }
 
     override fun getMatchList(leagueName: String) {
         view.showLoading()
-        compositeDisposable.add(matchRepositoryImplementation.getLastMatch(leagueName)
-            .observeOn(scheduler.ui())
-            .subscribeOn(scheduler.io())
-            .subscribeWith(object: ResourceSubscriber<EventResponse>(){
-                override fun onComplete() { view.hideLoading() }
+        compositeDisposable.add(
+            matchRepositoryImplementation.getLastMatch(leagueName)
+                .observeOn(scheduler.ui())
+                .subscribeOn(scheduler.io())
+                .subscribeWith(object : ResourceSubscriber<EventResponse>() {
+                    override fun onComplete() {
+                        view.hideLoading()
+                    }
 
-                override fun onNext(t: EventResponse) { view.showMatchList(t.events) }
+                    override fun onNext(t: EventResponse) {
+                        view.showMatchList(t.events)
+                    }
 
-                override fun onError(t: Throwable?) {
-                    view.hideLoading()
-                    view.showMatchList(Collections.emptyList())
-                }
-            })
+                    override fun onError(t: Throwable?) {
+                        view.hideLoading()
+                        view.showMatchList(Collections.emptyList())
+                    }
+                })
         )
     }
 }
